1f5aafb56ccaec13dad97175ffa42d9c89ee32c8
Dimas Saputro <codedimas@gmail.com>
2023-04-27 16:45:58 +0700

    Belajar struct

--- chapter-2/main.go
+++ chapter-2/main.go
@@ -13,6 +13,12 @@ func belajarDefer() {
 	fmt.Print("adalah ")			// 2
 }
 
+// 03 - struct
+type WebApp struct {
+	backend string
+	frontend string
+}
+
 func main() {
 	// 01 - defer
 	belajarDefer()
@@ -34,4 +40,27 @@ func main() {
 	*alamatRumahDimas = "Semarang"
 	fmt.Println("Dimas sekarang pindah ke", *alamatRumahDimas)
 	fmt.Println(rumahDimas)
+
+	// 03 - struct
+	fmt.Println(WebApp{"Golang", "Vue"})
+
+		// mengakses bagian struct
+		iClinicWeb := WebApp{"Laravel", "Angular Js"}
+		fmt.Println("iClinic Web menggunakan backend", iClinicWeb.backend, ", dan frontend", iClinicWeb.frontend)
+
+		// pointer struct
+		pointeriClinic := &iClinicWeb
+		// syntax (*pointeriClinic).backend sama dengan => pointeriClinic.backend
+		fmt.Println((*pointeriClinic).backend)
+		fmt.Println(pointeriClinic.backend) // prefer this
+		pointeriClinic.backend = "Golang"
+		fmt.Println(pointeriClinic.backend)
+
+		// inisialisasi struct
+		j := WebApp{"Laravel", "React"}		// j bertipe WebApp
+		k := WebApp{backend: "Firebase"}	// frontend = ""
+		l := WebApp{}						// backend = "", frontend = ""
+		pointerWeb := &WebApp{}				// pointerWeb bertipe *WebApp
+		fmt.Println(j, k, l, pointerWeb)
+
 }
