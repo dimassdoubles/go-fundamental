2e84c0e288169987d1522ab76b807e1d70d9cde5
Dimas Saputro <codedimas@gmail.com>
2023-04-27 16:24:10 +0700

    Belajar pointer

--- chapter-2/main.go
+++ chapter-2/main.go
@@ -4,13 +4,34 @@ import (
 	"fmt"
 )
 
-func main() {
-	// 01 - defer
+func belajarDefer() {
 	// output -> dimas adalah programmer baru di sts
 	fmt.Print("dimas ")				// 1
 	defer fmt.Println("di sts")		// 5
 	defer fmt.Print("baru ")		// 4
 	defer fmt.Print("programmer ")	// 3
 	fmt.Print("adalah ")			// 2
+}
 
+func main() {
+	// 01 - defer
+	belajarDefer()
+
+	// 02 - pointer
+	// pointer = penyimpan alamat ( *T )
+	var alamatRumahDimas *string
+
+	// & = "address-of" operator
+	rumahDimas := "Grobogan"
+	alamatRumahDimas = &rumahDimas
+
+	// *pointerVariable = mendapatkan 
+	// nilai dari alamat yang 
+	// di simpan pointerVariable
+	fmt.Println("Rumah dimas ada di", *alamatRumahDimas)
+
+	// dimas pindah rumah
+	*alamatRumahDimas = "Semarang"
+	fmt.Println("Dimas sekarang pindah ke", *alamatRumahDimas)
+	fmt.Println(rumahDimas)
 }
