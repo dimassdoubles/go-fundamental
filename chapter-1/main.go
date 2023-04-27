14b5c452bf1d5949670041e2a86dbe4bd11895ed
Dimas Saputro <codedimas@gmail.com>
2023-04-27 13:10:31 +0700

    Belajar package, import

--- chapter-1/main.go
+++ chapter-1/main.go
@@ -1,8 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
+	// 01 - hello world
 	fmt.Println("Hello World!")
+
+	// 02 - package, import
+	fmt.Println("Bilangan kesukaan saya adalah", rand.Intn(10))
+
 }
 
