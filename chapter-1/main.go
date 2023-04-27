7bd5bd27bc6fe18521234fb9af549427ac05ff47
Dimas Saputro <codedimas@gmail.com>
2023-04-27 13:15:35 +0700

    Belajar fungsi

--- chapter-1/main.go
+++ chapter-1/main.go
@@ -5,12 +5,19 @@ import (
 	"math/rand"
 )
 
+func add(x int, y int) int {
+	return x + y
+}
+
 func main() {
 	// 01 - hello world
 	fmt.Println("Hello World!")
 
 	// 02 - package, import
 	fmt.Println("Bilangan kesukaan saya adalah", rand.Intn(10))
+
+	// 03 - function
+	fmt.Println(add(25, 31))
 
 }
 
